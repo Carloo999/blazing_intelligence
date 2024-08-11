a34733fcb47c92af39fab545c4e6dc4ac4ae0133
Jan Kunde <jankunde00@gmail.com>
2024-08-11 16:40:43 +0200

    added dynamic learning rate adjuster

--- src/models/model_management/learning_rate_adjuster_enum.rs
+++ src/models/model_management/learning_rate_adjuster_enum.rs
@@ -1,17 +1,19 @@
-use crate::models::learning_rate::base_learning_rate_adjusters::{StepAdjust, ExpAdjust};
+use crate::models::learning_rate::base_learning_rate_adjusters::{StepAdjust, ExpAdjust, DynAdjust};
 use crate::models::learning_rate::learning_rate_adjuster::LearningRateAdjuster;
 
 #[derive(Savefile)]
 pub enum LearningRateAdjusterEnum {
     StepAdjust(StepAdjust),
     ExpAdjust(ExpAdjust),
+    DynAdjust(DynAdjust),
 }
 
 impl LearningRateAdjusterEnum {
     pub fn convert_to_learning_rate_adjuster(&self) -> Box<dyn LearningRateAdjuster> {
-        match self {
-            LearningRateAdjusterEnum::StepAdjust(x) => return Box::new(x.clone()),
-            LearningRateAdjusterEnum::ExpAdjust(x) => return Box::new(x.clone()),
+        return match self {
+            LearningRateAdjusterEnum::StepAdjust(x) => Box::new(x.clone()),
+            LearningRateAdjusterEnum::ExpAdjust(x) => Box::new(x.clone()),
+            LearningRateAdjusterEnum::DynAdjust(x) => Box::new(x.clone()),
         }
     }
 }
